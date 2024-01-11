f29c93e4d4248e9751948a47cfb337d1806c6b6f
Evanilson Pereira <pevanilson99@gmail.com>
2024-01-11 11:33:46 -0300

    Creating about page

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 // ROUTES
-
 router.get('/', (req, res) => {
-    res.render('index');
+    const locals = {
+        title: 'Node Js Blog',
+        description: 'Simple blog created with node, express and mongodb'
+    };
+
+    res.render('index', {locals, });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 module.exports = router;
