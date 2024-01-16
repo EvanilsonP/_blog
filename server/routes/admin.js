d747c1e654d333b433fc701e7ecd73b8dd7298e1
Evanilson Pereira <pevanilson99@gmail.com>
2024-01-16 11:48:00 -0300

    Creating User Schema

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
-
+const User = require('../models/User');
 
 const AdminLayout = '../views/layouts/admin';
 
